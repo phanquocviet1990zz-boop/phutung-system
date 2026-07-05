be1648c0d47e3d94b60995bac24e2ef1a00b0cfc
Phan Viet <phanquocviet1990zz@gmail.com>
2026-07-05 16:03:27 +0700

    deploy railway

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const cors = require("cors");
 const { getProductCodes, getImages } = require("./services/googleDrive");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ======= THƯ MỤC ẢNH GỐC =======
 
